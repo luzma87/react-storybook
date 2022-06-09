24512394b41dc488f79132ac0fc78a1402bc93b9
Luz Unda <lmunda@thoughtworks.com>
2022-06-09 13:54:18 -0500

    mejora menuItem

--- src/components/atoms/MenuItem/MenuItem.jsx
+++ src/components/atoms/MenuItem/MenuItem.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './menuItem.css'
+import {Typography} from '../Typography/Typography'
 
-const MenuItem = ({label, isSelected}) => {
+const MenuItem = ({label, isSelected, onClick}) => {
   return (
     <div className="menu-item">
-      <span>{label}</span>
+      <button onClick={onClick}>
+        <Typography variant="button">
+          {label}
+        </Typography>
+      </button>
       {isSelected && (<div className="menu-item-selected-marker" />)}
     </div>
   )
 }
 
 MenuItem.defaultProps = {
-  isSelected: false
+  isSelected: false,
+  onClick: undefined
 }
 
 MenuItem.propTypes = {
   label: PropTypes.string.isRequired,
-  isSelected: PropTypes.bool
+  isSelected: PropTypes.bool,
+  onClick: PropTypes.func
 }
 
 export default MenuItem
